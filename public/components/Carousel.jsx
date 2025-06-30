0a7a3dbfd279a3d954df4254e0a6d13e9024a1f5
Carlos Morin <carlos.alberto.morin@gmail.com>
2025-06-30 01:19:42 -0300

    Update: Se agregan link de las redes sociales. Se cambia el menu del header y se agrega el buscador. Se soluciona el problema de visualizacion del carousel.

--- public/components/Carousel.jsx
+++ public/components/Carousel.jsx
@@ -9,6 +9,13 @@ import 'swiper/css/effect-fade';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 import './Carousel.css';
+//Import image
+import BlackAndWhite from '../../src/assets/black-and-white.jpg';
+import Clothes from '../../src/assets/clothes.jpg';
+import Woman from '../../src/assets/woman.jpg';
+import BlackAndWhiteMobile from '../../src/assets/black-and-white-mobile.jpg';
+import ClothesMobile from '../../src/assets/clothes-mobile.jpg';
+import WomanMobile from '../../src/assets/woman-mobile.jpg';
 
 function Carousel() {
     const progressCircle = useRef(null);
@@ -36,20 +43,20 @@ function Carousel() {
          >
         <SwiperSlide>
             <picture>                
-                <source media="(max-width: 425px)" srcset="../../src/assets/black-and-white-mobile.jpg" alt="black-and-white-mobile"/>
-                <img src="../../src/assets/black-and-white.jpg" alt="black-and-white"/>
+                <source media="(max-width: 425px)" srcset={BlackAndWhiteMobile} alt="black-and-white-mobile"/>
+                <img src={BlackAndWhite} alt="black-and-white"/>
             </picture>
         </SwiperSlide>
         <SwiperSlide>
             <picture>                
-                <source media="(max-width: 425px)" srcset="../../src/assets/clothes-mobile.jpg" alt="clothes-mobile"/>
-                <img src="../../src/assets/clothes.jpg" alt="clothes" />
+                <source media="(max-width: 425px)" srcset={ClothesMobile} alt="clothes-mobile"/>
+                <img src={Clothes} alt="clothes" />
             </picture>            
         </SwiperSlide>
         <SwiperSlide>
             <picture>                
-                <source media="(max-width: 425px)" srcset="../../src/assets/woman-mobile.jpg" alt="woman-mobile"/>
-                <img src="../../src/assets/woman.jpg" alt="woman" />
+                <source media="(max-width: 425px)" srcset={WomanMobile} alt="woman-mobile"/>
+                <img src={Woman} alt="woman" />
             </picture> 
         </SwiperSlide>          
         <div className="autoplay-progress" slot="container-end">
